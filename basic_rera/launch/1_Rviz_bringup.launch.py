c466d370d126c279c96362161273accddcb0c5b0
luqman <noshluk2@gmail.com>
2021-05-20 11:29:20 +0500

    Basic rera needs urdf setting , autonomous package is not completed

---
+++ basic_rera/launch/1_Rviz_bringup.launch.py
@@ -0,0 +1,31 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('urdf_package')
+    urdf = os.path.join(package_dir,'1_caster_2_wheel.urdf')
+    rviz_config_file=os.path.join(package_dir,'caster_2_wheel.rviz')
+    
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            arguments=[urdf]),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            arguments=[urdf]),
+        Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d',rviz_config_file],
+        output='screen'),
+        
+
+    ])
